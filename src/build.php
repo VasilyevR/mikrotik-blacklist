efa2ccbbcf9757245e3c8b7e7814317ccd644acc
VasilyevR <romanvasilyev@gmail.com>
2021-03-04 13:22:36 +0300

    Add files via upload

--- src/build.php
+++ src/build.php
@@ -1,15 +1,10 @@
 <?php
-declare(strict_types=1);
 
 namespace App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$lists = [
-        'normshield_all_attack.ipset',
-        'normshield_high_attack.ipset',
-        'firehol_level3.netset',
-        'ciarmy.ipset',
-    ];
-$blacklist = new BlackList\BlackList($lists);
-$blacklist->getList();
+$fileListsContent = file_get_contents(__DIR__ . '/' . 'lists.txt');
+$lists = explode("\n", $fileListsContent);
+$blacklist = new BlackList\Builder(__DIR__ . '/../blocklist-ipsets/', $lists);
+$blacklist->buildFile(__DIR__ . '/../blocklist.rsc');
