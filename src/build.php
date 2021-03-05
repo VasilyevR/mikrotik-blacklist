2f76ecd79e8589573a8f5a424d79ae2aa9a65c6f
VasilyevR <romanvasilyev@gmail.com>
2021-03-05 20:02:01 +0300

    Add files via upload

--- src/build.php
+++ src/build.php
@@ -7,4 +7,4 @@ require __DIR__ . '/../vendor/autoload.php';
 $lists = file(__DIR__ . '/' . 'lists.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $realPath = realpath(__DIR__ . '/../blocklist-ipsets/') . DIRECTORY_SEPARATOR;
 $blacklist = new BlackList\Builder($realPath, $lists);
-$blacklist->buildFile(__DIR__ . '/../blocklist.rsc');
+$blacklist->buildFile(__DIR__ . '/../blocklist');
